dcecd31b4f823d445df5abd6fe40cb7363eb529a
Юлия Корюкина <koryuckina.yu@yandex.ru>
2026-09-18 19:59:03 +0200

    feat: js script for Toggle theme

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("moon").onclick = function () {
+    document.body.classList.add("dark");
+  };
+  document.getElementById("sun").onclick = function () {
+    document.body.classList.remove("dark");
+  };
